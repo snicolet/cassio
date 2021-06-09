e671ce296508d2dc4b3a5452633c6cc8e578d9e6
Stéphane Nicolet <cassio@free.fr>
2021-06-10 00:33:58 +0200

    Introduce execute_carbon_protocol() function

--- src/carbon/carbon.py
+++ src/carbon/carbon.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-# File carbon.py : a library to attack Qt using a text protocol,
+# File carbon.py: a library to attack Qt using a text protocol,
 #                  using the standard input and output.
 #
 #
-# Usage :
+# Usage:
 #     python3 carbon.py
-#     python3 carbon.py [-echo] [-echo_input] [-echo_output] [-file name]
+#     python3 carbon.py [-file name] [-echo] [-echo_input] [-echo_output] [-colored] 
 #
 
 
@@ -25,12 +25,13 @@ from PyQt5.Qt        import Qt
 # Step 2. Set global variables by analyzing the command line arguments
 
 script_args       = sys.argv[1:]                   # the list of arguments to the script
-echo              = "-echo"        in script_args  # True or False
-echo_input        = "-echo_input"  in script_args  # True or False
-echo_output       = "-echo_output" in script_args  # True or False
+echo              = "-echo"        in script_args  # flag to echo both input and output
+echo_input        = "-echo_input"  in script_args  # flag to echo only input
+echo_output       = "-echo_output" in script_args  # flag to echo only output
+colored           = "-colored"     in script_args  # flag to use colored echo (need a Terminal with ANSI support)
 
 input_file_name = ""                               # the name of the script to be played
-if "-file" in script_args :
+if "-file" in script_args:
    f = script_args.index("-file")
    if f >= 0:
       input_file_name = script_args[f+1]
@@ -38,6 +39,15 @@ if "-file" in script_args :
 line_counter      = 0                              # a global counter for the lines received by the GUI server
 PROTOCOL_PREFIX   = "CARBON-PROTOCOL "             # prefix for the protocol commands
 
+
+class colors:
+    """ A class to declare constants for colored ANSI Codes """
+
+    OK      = '\033[92m'  # GREEN
+    WARNING = '\033[93m'  # YELLOW
+    FAIL    = '\033[91m'  # RED
+    RESET   = '\033[0m'   # RESET COLOR
+    
 
 # Step 3. Let's program the server
 
@@ -67,10 +77,10 @@ class StandardInputThread(threading.Thread):
         answer = "OK"
 
         # loop and wait to get input + Return
-        while answer == "OK" :
+        while answer == "OK":
             answer = self.callback(input())
 
-        if answer == "quit" :
+        if answer == "quit":
             # hard exit of the whole process without calling
             # cleanup handlers, flushing stdio buffers, etc.
             os._exit(0)
@@ -88,10 +98,10 @@ def server_callback(line):
     line_counter += 1
     message = 'GUI [{:4d}] < {}'.format(line_counter, line.strip())
     dispatch_message(message)
-    if line == "quit" :
+    if line == "quit":
         print("...quitting the Carbon-GUI server, bye...")
         return "quit"
-    else :
+    else:
         return "OK"
 
 
@@ -99,9 +109,9 @@ def read_input_file(name):
    """
    Read the given file, and send it line by line to the server
    """
-   if name != "" :
-      with open(name) as fp :
-         for line in fp :
+   if name != "":
+      with open(name) as fp:
+         for line in fp:
             simulate_server_line(line)
 
 
@@ -122,6 +132,42 @@ def simulate_carbon_gui(line):
 
 
 
+def execute_carbon_protocol(command, args):
+    """
+    This is the core of the library, which transforms the commands of the 
+    CARBON-PROTOCOL into real Qt objects and calls
+    """
+    
+    # A lambda to tag a command as not implemented yet.
+    not_implemented = lambda : print("NOT IMPLEMENTED: {}".format(command))
+
+    # Should we echo each line?
+    if echo or echo_output:
+        if colored:
+           print(colors.OK + "GUI [exec] >", command, args, colors.RESET)
+        else:
+           print("GUI [exec] >", command, args)
+
+    # A long switch for the various commands, implementing each command with Qt.
+    # 
+    # Note: most common commands should be near the top for better performance.
+    
+    if command == "get-mouse"    : 
+        not_implemented()
+    elif command == "new-window" : 
+       not_implemented()
+    elif command == "set-window-title"  : 
+       not_implemented()
+    elif command == "set-window-geometry"  : 
+       not_implemented()
+    elif command == "show-window"  : 
+       not_implemented()
+    else:
+       not_implemented()
+    
+       
+
+
 def dispatch_message(message):
    """
    Interprets the message with the Carbon Gui protocol.
@@ -130,21 +176,24 @@ def dispatch_message(message):
      python3 carbon.py -echo -file torture.txt
    """
    lines = message.splitlines()
-   for line in lines :
+   for line in lines:
       line = line.strip()
       occ = line.find(PROTOCOL_PREFIX)
 
-      if occ < 0 :
-         if echo or echo_input :
-            print(bcolors.FAIL + line + bcolors.RESET)
+      if occ < 0:
+         if echo or echo_input:
+            if colored:
+               print(colors.FAIL + line + colors.RESET)
+            else:
+               print(line)
 
-      if occ >= 0 :
+      if occ >= 0:
          lexems = csv_split(line[occ+len(PROTOCOL_PREFIX):len(line)])
-         if len(lexems) > 0 :
+         if len(lexems) > 0:
+         
             command = lexems[0]
             args    = lexems[1:]
-            if echo or echo_output :
-               print(bcolors.OK + "GUI [exec] >", command, args, bcolors.RESET)
+            execute_carbon_protocol(command, args)
 
 
 def csv_split(s):
@@ -159,22 +208,12 @@ def csv_split(s):
     return list(filter(lambda s: s != "", lexems))
 
 
-
-class bcolors:
-    """ A class to declare constants for colored ANSI Codes """
-
-    OK = '\033[92m' #GREEN
-    WARNING = '\033[93m' #YELLOW
-    FAIL = '\033[91m' #RED
-    RESET = '\033[0m' #RESET COLOR
-
-
 class HelloWorldWindow(QWidget):
     """
     A class (in Qt) to demonstrate the creation of an "About box" window
     """
 
-    def __init__(self) :
+    def __init__(self):
       """
         Constructor for the class
       """
@@ -182,7 +221,7 @@ class HelloWorldWindow(QWidget):
       self.initializeUI()
 
 
-    def initializeUI(self) :
+    def initializeUI(self):
       """
          Set the size and title of the window, and shows it
       """
@@ -192,7 +231,7 @@ class HelloWorldWindow(QWidget):
       self.show()
 
 
-    def displayLabels(self) :
+    def displayLabels(self):
       """
       Displays a small text and an image in the window.
       """
@@ -204,12 +243,14 @@ class HelloWorldWindow(QWidget):
 
       # Check to see if image files exist, if yes show it, otherwise throw an exception
       image = "images/world.png"
-      try :
+      try:
          with open(image):
-            world_image = QLabel(self)
             pixmap = QPixmap(image).scaledToHeight(200, Qt.SmoothTransformation)
+            
+            world_image = QLabel(self)
             world_image.setPixmap(pixmap)
             world_image.move(25, 40)
+            
       except FileNotFoundError:
          print("Image not found.")
 
@@ -225,15 +266,15 @@ if __name__ == "__main__":
     # start the standard input thread
     input_thread = StandardInputThread(server_callback)
 
-    # read the (optional) input file, which should contain
-    # a sequence of lines using the CARBON-PROTOCOL
+    # read the (optional) input file
     read_input_file(input_file_name)
 
     # open the about box (this is programmed in Qt)
     window = HelloWorldWindow()
 
     # clean exit for the Qt app
-    sys.exit(app.exec_())
+    res = app.exec_()
+    sys.exit(res)
 
 
 
