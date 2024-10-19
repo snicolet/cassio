b9d2d426940b9dda8f3cb9ebba373bd8f3e9613b
Stéphane Nicolet <cassio@free.fr>
2024-10-19 23:46:01 +0200

    use locate to find python binaries

--- src/carbon/pyqt-check.py
+++ src/carbon/pyqt-check.py
@@ -1,7 +1,7 @@
 
 # File pyqt-check.py : check if the current python (called PYTHON-BINARY in the following)
 #                      has access to a PyQt module. This may be useful if you have many
-#                      python environment installed on your system.
+#                      python environments installed on your system.
 #
 # Usage :
 #     PYTHON-BINARY pyqt-check.py -PyQt4    
@@ -19,13 +19,13 @@ if (len(args) > 0) :
 if (lib.find("PyQt4") >= 0) :
 	try :
    		from PyQt4.QtGui import QApplication
-   		print("  --> installed : PyQt4 ")
+   		print("    --->   installed : PyQt4 ")
 	except :
    		pass
 
 if (lib.find("PyQt5") >= 0) :
 	try :
    		from PyQt5.QtWidgets import QApplication
-   		print("  --> installed : PyQt5 ")
+   		print("    --->   installed : PyQt5 ")
 	except :
    		pass
--- src/carbon/pyqt-search.py
+++ src/carbon/pyqt-search.py
@@ -1,43 +1,83 @@
 
-# File pyqt-search.py : search all your Python installations in /opt/local/bin/ , then
-#                       check for each one if the module PyQt4 or the module PyQt5 is
-#                       installed.
+# File pyqt-search.py : this script lists all your Python installations in your 
+#                       system, then for each Python binary found, it checks if the 
+#                       PyQt4 module or the PyQt5 module is installed (and usable).
 #
 # Usage :
 #     python pyqt-search.py
 #
 #
 
-
-
 import os
 import re
 
-# Get the list of the potential Python binaries on my system
-cmd = "ls -a /opt/local/bin/ | grep python3 > list-of-python-installations.txt"
-value = os.system(cmd)
+tested = []
+
+def check_binaries(filename, remove_alias) :
+
+    with open(filename, "r") as input: 
+        for line in input :
+   
+            if (line.find("-config") >= 0) :
+                continue
+
+            if (remove_alias) :
+                pos = line.find("@")
+                if (pos > 0) :
+                    line = line[0:pos]
+
+            binary = re.sub("[^a-z0-9-./@]+","", line, flags=re.IGNORECASE)
+            
+            binary = path + binary
+            
+            # print("binary = ", binary)
+            # print("line = ", line)
+            
+            if (binary.find("/bin/") < 0) :
+                continue
+            
+            if (binary.find("/bin/python3") < 0) :
+                continue
+            
+            if not(os.path.exists(binary)) :
+                continue
+            
+            if binary in tested :
+                continue
+            else :
+                tested.append(binary)
+            
+            # print name of Python binary
+            print(binary)
+          
+            # call that Python executable with pyqt-check.py
+            value = os.system(binary + " pyqt-check.py  -PyQt4 ")
+            value = os.system(binary + " pyqt-check.py  -PyQt5 ")
+            
+            if os.path.exists(filename):
+                 os.remove(filename)
 
 print()
 
-with open("list-of-python-installations.txt", "r") as input: 
-   for line in input :
-   
-      if (line.find("-config") >= 0) :
-          pass
+# Get the list of the potential Python binaries on my system,
+# using some traditionnal locations
+paths = [ "/usr/local/lib/", "/opt/local/bin/" ]
 
-      else :
-              
-          pos = line.find("@")
-          if (pos > 0) :
-              line = line[0:pos]
+for path in paths :
+    filename = "list-of-python-installations.txt"
+    cmd = "ls -a " + path + " | grep python3 > " + filename
+    value = os.system(cmd)
+    check_binaries(filename, True)
 
-          line = re.sub("[^a-z0-9-.]+","", line, flags=re.IGNORECASE)
-          
-          # print name of Python executable
-          print(line)
-          
-          # call that Python executable with pyqt-check.py
-          value = os.system(line + " pyqt-check.py  -PyQt4 ")
-          value = os.system(line + " pyqt-check.py  -PyQt5 ")
-         
+# Get the list of the potential Python binaries on my system,
+# using the locate function (if available)
+filename = "list-of-python-installations-locate.txt"
+path = ""
+value = os.system("which locate > " + filename)
+if (value == 0) :
+    cmd = "locate python3 | grep /bin/ > " + filename
+    value = os.system(cmd)
+    check_binaries(filename, False)
+
+
 print()
